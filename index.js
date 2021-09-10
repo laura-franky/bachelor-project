ae03145d3da49df8b41c916ea87e6cba027760ea
laura hontabat-franky <laura.hontabat-franky@incloud.de>
2021-09-10 14:43:37 +0200

    reworked recipe app and simplified it

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import express, { Router } from "express";
+
+// example data
 export const recipes = [
   {
     name: "Blaubeer Muffins",
@@ -63,3 +66,46 @@ export const recipes = [
     ],
   },
 ];
+
+const port = 3000;
+export const globalRouter = Router({ mergeParams: true });
+
+// Basic get
+globalRouter.get("/", async (_, res) => {
+  res.send({
+    message: "Willkommen",
+  });
+});
+
+// Basic get
+globalRouter.get("/recipes", async (_, res) => {
+  res.send({
+    message: recipes,
+  });
+});
+
+globalRouter.get("/recipes/:id", async (req, res) => {
+  const id = Number(req.params.id) - 1;
+  if (typeof recipes[id] === "undefined") {
+    res.status(404).send({
+      message: "recipe not found",
+    });
+  } else {
+    res.send({
+      message: recipes[id],
+    });
+  }
+});
+
+export const startServer = () => {
+  const app = express();
+
+  app.use("/", globalRouter);
+
+  const server = app.listen(port, () =>
+    console.log("Server is running on port", port)
+  );
+  return { server };
+};
+
+startServer();
